809569c04c7bfd2d51d27674f4b4fd39cb5c0d5d
kgbolton <kbolton@andrew.cmu.edu>
2019-09-27 12:53:12 -0400

    Data formatted.
    Tried to add context to what the data is conceptually exploring in an infobox. Cannot for the life of me get the infobox to span a larger width. Moving on for now.

--- FLvotes/app.R
+++ FLvotes/app.R
@@ -10,9 +10,30 @@ library(ggplot2)
 library(shinythemes)
 
 # Format data
+turnout <- read.csv("florida_turnout.csv")
+colnames(turnout)[1] <- "Year"
+elections <- read.csv("florida_elections.csv")
+colnames(elections)[1] <- "ElectionDate"
+voter_reg <- read.csv("florida_voter_reg_Aug_19.csv")
+
+## for datatable
+formatElections <- elections %>%
+    filter((RaceCode=="GOV" | RaceCode=="PRE" | RaceCode=="USS") & PartyName != "Write-In") %>%
+    group_by(ElectionDate, RaceCode, CanNameLast, CanNameFirst) %>%
+    summarise(VotesReceived = sum(CanVotes))
+electionsTable1 <- left_join(formatElections, elections[,c(1,3,5,12:14)], by = c("CanNameLast", "CanNameFirst", "ElectionDate"))
+electionsTable <- distinct(electionsTable1[,c(1,7,4,8,3,6,5)])
+electionsTable$ElectionDate <- relevel(electionsTable$ElectionDate, "11/8/2016")
+electionsTable$ElectionDate <- relevel(electionsTable$ElectionDate, "11/6/2018")
+electionsTable <- arrange(electionsTable, ElectionDate, OfficeDesc, desc(VotesReceived))
+## totals that will be used for later computations
+electionTotals <- formatElections %>%
+    ungroup() %>%
+    group_by(ElectionDate, RaceCode) %>%
+    summarise(electionTotal = sum(VotesReceived))
 
 # Header and title
-header <- dashboardHeader(title = "Florida Statewide Elections",
+header <- dashboardHeader(title = "Florida Statewide Elections"
                           #stuff)
                           
 )
@@ -21,11 +42,34 @@ header <- dashboardHeader(title = "Florida Statewide Elections",
 sidebar <- dashboardSidebar(
     sidebarMenu(
         id = "tabs",
+        
+        # First Menu Item
+        menuItem("Charts", icon = icon("bar-chart"), tabName = "plot"),
+        
+        # Inputs to plots
+        
+        #Second Menu Item
+        menuItem("Table", icon = icon("table"))
     )
 )
 
 # Dashboard body
 body <- dashboardBody(tabItems(
+    
+    # Plot page
+    tabItem("plot",
+            # info box
+            fluidRow(column(24, 
+                infoBoxOutput("context")
+            )),
+            # value boxes
+            fluidRow(
+                valueBoxOutput("pMargins"),
+                valueBoxOutput("rawMargins")
+            )
+            
+            #
+            )
     
     # stuff
 ))
@@ -35,7 +79,12 @@ ui <- dashboardPage(header, sidebar, body)
 # Define server function required to create plots and value boxes
 server <- function(input, output) {
 
-    #stuff
+    # Context info box ----------------------------------------------
+    output$context <- renderInfoBox({
+        infoBox(title = "", value = "Amendment 4", subtitle = "In November 2018, Florida voters passed a ballot measure to return the right to vote to former felons having met certain criteria. This measure launched one of the largest enfranchisement efforts in modern U.S. history, with more than 1 million people potentially eligible to regain their right to vote in a state with a population of 21 million.", icon = icon("ok", lib = "glyphicon"), color = "blue")
+    })
+    
+
 }
 
 # Run the application 
